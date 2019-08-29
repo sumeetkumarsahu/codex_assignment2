be1caf74d4892937745e7cc6b006ae3a4c3e51f3
Sumeet Kumar Sahu <53472581+sumeetkumarsahu@users.noreply.github.com>
2019-08-29 09:58:39 +0530

    Create question_6.java

---
+++ question_6.java
@@ -0,0 +1,26 @@
+package codex_assignment2;
+
+public class Question_6 
+{
+
+	public static void main(String[] args) 
+	{
+		int i,j,num1,sum1=0,diff=0;
+		int sum2=0;
+		for(i=1;i<=100;i++)
+		{
+		    num1=i*i;
+		    sum1=sum1+num1;
+		    
+		    }
+		for(i=1;i<=100;i++)
+		{
+			sum2=sum2+i;
+			
+		}
+		   
+		    System.out.println("diffrence is"+((sum2*sum2)-sum1));
+			
+		}
+
+}
